c19d409f696c1a28a712759ba601d607024d3029
david <github@djs.gg>
2022-11-20 17:23:28 -0500

    commit current backup/restore scripts

--- backup.sh
+++ backup.sh
@@ -1,3 +1,26 @@
-pacman -Qqen
+pacman -Qqen > pacman-packages.txt
 
-pacman -Qqem
+pacman -Qqem > aur-packages.txt
+
+read -p "Backup Destination: " BACKUPDEST
+BACKUPPATH=$BACKUPDEST/$USER-backup
+mkdir -p $BACKUPPATH$HOME
+BACKUPPATH=$BACKUPDEST/$USER-backup/
+
+mkdir -p $BACKUPDEST$HOME
+rsync -aHv --progress --preallocate $HOME $BACKUPDEST/$USER-backup/home/
+
+sudo mkdir -p $BACKUPPATH/root/etc
+
+sudo cp -r /etc/cups $BACKUPPATH/root/etc/
+
+sudo cp /etc/pacman.conf $BACKUPPATH/root/etc/
+
+sudo cp /etc/oblogout.conf $BACKUPPATH/root/etc/
+
+sudo mkdir -p $BACKUPPATH/root/etc/fonts
+
+sudo cp /etc/fonts/local.conf $BACKUPPATH/root/etc/fonts/
+sudo cp -r /etc/fonts/conf.avail $BACKUPPATH/root/etc/fonts/
+
+find /etc/fonts/conf.d -name '*.conf' -printf "%f\n" > fontconfig-entries.txt
--- restore.sh
+++ restore.sh
@@ -1,1 +1,7 @@
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -Syu --needed - < pacman-packages.txt
+
+sudo pacman -Syu --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+yay -Syu --needed - < aur-packages.txt
+
+for file in $(<fontconfig-entries.txt); do ln -s /usr/share/fontconfig/conf.avail/"$file" /etc/fonts/conf.d/; done
