52c1cc410ab8ae7868b59c2b6660c408dbcbf721
david <github@djs.gg>
2022-11-21 16:00:01 -0500

    push scripts

--- backup.sh
+++ backup.sh
@@ -1,14 +1,16 @@
+#! /bin/bash
 pacman -Qqen > pacman-packages.txt
 
 pacman -Qqem > aur-packages.txt
 
 read -p "Backup Destination: " BACKUPDEST
 BACKUPPATH=$BACKUPDEST/$USER-backup
-mkdir -p $BACKUPPATH$HOME
-BACKUPPATH=$BACKUPDEST/$USER-backup/
 
-mkdir -p $BACKUPDEST$HOME
-rsync -aHv --progress --preallocate $HOME $BACKUPDEST/$USER-backup/home/
+sudo mkdir -p $BACKUPPATH/root/var/cache/pacman/
+sudo rsync -avHAXSU --progress --preallocate /var/cache/pacman/pkg  $BACKUPPATH/root/var/cache/pacman/
+
+sudo mkdir -p $BACKUPPATH/home
+sudo rsync -avHAXSU --progress --preallocate $HOME $BACKUPPATH/home/
 
 sudo mkdir -p $BACKUPPATH/root/etc
 
@@ -24,3 +26,15 @@ sudo cp /etc/fonts/local.conf $BACKUPPATH/root/etc/fonts/
 sudo cp -r /etc/fonts/conf.avail $BACKUPPATH/root/etc/fonts/
 
 find /etc/fonts/conf.d -name '*.conf' -printf "%f\n" > fontconfig-entries.txt
+
+sudo cp -r /etc/lightdm/ $BACKUPPATH/root/etc/
+
+sudo cp -r /etc/X11/ $BACKUPPATH/root/etc/
+
+sudo cp /etc/passwd $BACKUPPATH/root/etc/
+sudo cp /etc/group $BACKUPPATH/root/etc/
+
+sudo cp /etc/sudoers $BACKUPPATH/root/etc/
+
+sudo mkdir -p $BACKUPPATH/root/usr
+sudo cp -r /usr/share/fonts /usr/share/icons /usr/share/themes/ /usr/share/pixmaps $BACKUPPATH/root/usr/
--- restore.sh
+++ restore.sh
@@ -1,7 +1,31 @@
-sudo pacman -Syu --needed - < pacman-packages.txt
+read -p "Root path of backup (location of the folder \$USER-backup)?: " BACKUPLOC
+BACKUPPATH=$BACKUPLOC/$USER-backup
 
-sudo pacman -Syu --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -Syu --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+
+sudo cp $BACKUPPATH/root/etc/pacman.conf /etc/
+
+sudo rm /etc/X11/xinit/xinitrc.d/40-libcanberra-gtk-module.sh
+
+sudo pacman -Syu --needed - < pacman-packages.txt
 
 yay -Syu --needed - < aur-packages.txt
 
+sudo rsync -avHAXSU --progress --preallocate $BACKUPPATH/home/$USER /home/
+
+sudo cp -r $BACKUPPATH/root/etc /
+
+sudo cp -r $BACKUPPATH/root/usr /
+
+sudo rm /etc/fonts/conf.d/*
 for file in $(<fontconfig-entries.txt); do ln -s /usr/share/fontconfig/conf.avail/"$file" /etc/fonts/conf.d/; done
+
+find /etc/systemd/user/ -type f -regex '.*\.\(service\|timer\)$' -exec systemctl enable --user "{}" \;
+
+sudo systemctl enable NetworkManager.service
+
+sudo systemctl enable lightdm.service
